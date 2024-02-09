e37789f1b253dd7c249c051bebd3ff734a5159ed
Helena Sánchez <helena.sangar@gmail.com>
2024-02-10 00:05:05 +0100

    add budget modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Button, Container, Stack } from 'react-bootstrap'
-import './App.css'
 import BudgetCard from './components/BudgetCard'
+import AddBudgetModal from './components/AddBudgetModal'
+import { useState } from 'react'
+import { useBudgets } from './context/BudgetsContext'
+import './App.css'
 
 function App() {
 
-  const name = [
-    'Entertainment'
-  ]
+
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
+  const { budgets, getBudgetExpenses } = useBudgets()
+
+  const handleClose = () => {
+    setShowAddBudgetModal(!showAddBudgetModal)
+  }
+
+  const toggleAddBudgetModal = () => {
+    setShowAddBudgetModal(true)
+  }
+
 
   return (
 
@@ -15,7 +27,7 @@ function App() {
 
         <Stack direction='horizontal' gap={2} className='mb-4'>
           <h1 className="me-auto">Budgets</h1>
-          <Button variant='primary'>Add Budget</Button>
+          <Button variant='primary' onClick={toggleAddBudgetModal}>Add Budget</Button>
           <Button variant='outline-primary'>Add expenses</Button>
           <div
             style={{
@@ -28,9 +40,27 @@ function App() {
           </div>
         </Stack>
 
-        <BudgetCard name={name} amount={190} max={1500} gray />
+        {budgets.map(budget => {
+
+          const amount = getBudgetExpenses(budget.id).reduce(
+            (total, expense) => total + expense.amount,
+            0)
+
+          return (
+
+            <BudgetCard
+              key={budget.id}
+              name={budget.name}
+              amount={amount}
+              max={budget.max}
+            />
+
+          )
+        })}
 
       </Container>
+
+      <AddBudgetModal show={showAddBudgetModal} handleClose={handleClose} />
     </>
   )
 }
--- src/context/BudgetsContext.jsx
+++ src/context/BudgetsContext.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react"
 import { v4 as uuidV4 } from "uuid"
-import useLocalStorage from "../components/hooks/useLocalStorage"
+import useLocalStorage from "../hooks/useLocalStorage"
+
 
 const BudgetsContext = React.createContext()
 
