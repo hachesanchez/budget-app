a88fffc4fa964dedf09c5b9df7a398768f38f9ee
Helena Sánchez <helena.sangar@gmail.com>
2024-02-09 20:03:53 +0100

    html, css, budget context, local storage hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button, Container, Stack } from 'react-bootstrap'
 import './App.css'
+import BudgetCard from './components/BudgetCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name = [
+    'Entertainment'
+  ]
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container className='my-4'>
+
+        <Stack direction='horizontal' gap={2} className='mb-4'>
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant='primary'>Add Budget</Button>
+          <Button variant='outline-primary'>Add expenses</Button>
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fill, min-max(300px, 1fr))',
+              gap: '1rem',
+              allignItems: 'flex-start'
+            }}
+          >
+          </div>
+        </Stack>
+
+        <BudgetCard name={name} amount={190} max={1500} gray />
+
+      </Container>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BudgetsProvider } from './context/BudgetsContext.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BudgetsProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </BudgetsProvider>
 )
